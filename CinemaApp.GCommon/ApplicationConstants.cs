d8a36721a06f0cbfbdcf86ffb6ed46cb2883f7ae
IEnum <135451084+svetlanasieber@users.noreply.github.com>
2025-07-18 00:39:47 +0200

    Add files via upload

---
+++ CinemaApp.GCommon/ApplicationConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace CinemaApp.GCommon
+{
+    public static class ApplicationConstants
+    {
+        public const string AppDateFormat = "yyyy-MM-dd";
+        public const string NoImageUrl = "no-image.jpg";
+        public const string IsDeletedPropertyName = "IsDeleted";
+        public const string PriceSqlType = "decimal(18, 6)";
+        public const string AccessDeniedPath = "/Home/AccessDenied";
+        public const string ManagerAuthCookie = "ManagerAuth";
+    }
+}
---
+++ CinemaApp.GCommon/ExceptionMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace CinemaApp.GCommon
+{
+    public static class ExceptionMessages
+    {
+        public const string InterfaceNotFoundMessage =
+            "The {0} could not be added to the Service Collection, because no interface matching the convention could be found! Convention for Interface naming: I{0}.";
+    }
+}
