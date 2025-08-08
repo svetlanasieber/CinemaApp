c501d0869d5639126f7d6a20dcd12f62e423a648
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-08-09 01:25:15 +0200

    Add files via upload
    service 09/08

---
+++ CinemaApp.Services.Core/CinemaService.cs
@@ -0,0 +1,103 @@
+﻿namespace CinemaApp.Services.Core
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using Data.Models;
+    using Data.Repository.Interfaces;
+    using Interfaces;
+    using Web.ViewModels.Cinema;
+
+    using static GCommon.ApplicationConstants;
+
+    public class CinemaService : ICinemaService
+    {
+        private readonly ICinemaRepository cinemaRepository;
+
+        public CinemaService(ICinemaRepository cinemaRepository)
+        {
+            this.cinemaRepository = cinemaRepository;
+        }
+
+        public async Task<IEnumerable<UsersCinemaIndexViewModel>> GetAllCinemasUserViewAsync()
+        {
+            IEnumerable<UsersCinemaIndexViewModel> allCinemasUsersView = await this.cinemaRepository
+                .GetAllAttached()
+                .Select(c => new UsersCinemaIndexViewModel()
+                {
+                    Id = c.Id.ToString(),
+                    Name = c.Name,
+                    Location = c.Location,
+                })
+                .ToArrayAsync();
+            
+            return allCinemasUsersView;
+        }
+
+        public async Task<CinemaProgramViewModel?> GetCinemaProgramAsync(string? cinemaId)
+        {
+            CinemaProgramViewModel? cinemaProgram = null;
+            if (!String.IsNullOrWhiteSpace(cinemaId))
+            {
+                Cinema? cinema = await this.cinemaRepository
+                    .GetAllAttached()
+                    .Include(c => c.CinemaMovies)
+                    .ThenInclude(cm => cm.Movie)
+                    .SingleOrDefaultAsync(c => c.Id.ToString().ToLower() == cinemaId.ToLower());
+                if (cinema != null)
+                {
+                    cinemaProgram = new CinemaProgramViewModel()
+                    {
+                        CinemaId = cinema.Id.ToString(),
+                        CinemaName = cinema.Name,
+                        CinemaData = cinema.Name + " - " + cinema.Location,
+                        Movies = cinema.CinemaMovies
+                            .Select(cm => cm.Movie)
+                            .DistinctBy(m => m.Id)
+                            .Select(m => new CinemaProgramMovieViewModel()
+                            {
+                                Id = m.Id.ToString(),
+                                Title = m.Title,
+                                ImageUrl = m.ImageUrl ?? $"/images/{NoImageUrl}",
+                                Director = m.Director
+                            })
+                            .ToArray(),
+                    };
+                }
+            }
+
+            return cinemaProgram;
+        }
+
+        public async Task<CinemaDetailsViewModel?> GetCinemaDetailsAsync(string? cinemaId)
+        {
+            CinemaDetailsViewModel? cinemaDetails = null;
+            if (!String.IsNullOrWhiteSpace(cinemaId))
+            {
+                Cinema? cinema = await this.cinemaRepository
+                    .GetAllAttached()
+                    .Include(c => c.CinemaMovies)
+                    .ThenInclude(cm => cm.Movie)
+                    .SingleOrDefaultAsync(c => c.Id.ToString().ToLower() == cinemaId.ToLower());
+                if (cinema != null)
+                {
+                    cinemaDetails = new CinemaDetailsViewModel()
+                    {
+                        Name = cinema.Name,
+                        Location = cinema.Location,
+                        Movies = cinema.CinemaMovies
+                            .Select(cm => cm.Movie)
+                            .DistinctBy(m => m.Id)
+                            .Select(m => new CinemaDetailsMovieViewModel()
+                            {
+                                Title = m.Title,
+                                Duration = m.Duration.ToString(),
+                            })
+                            .ToArray(),
+                    };
+                }
+            }
+
+            return cinemaDetails;
+        }
+    }
+}
