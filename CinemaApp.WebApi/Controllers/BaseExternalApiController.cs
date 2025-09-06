5acb4bcf28714fce0148088e6f327cdc89debde9
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-09-06 23:42:25 +0200

    controller _update_609

---
+++ CinemaApp.WebApi/Controllers/BaseExternalApiController.cs
@@ -0,0 +1,34 @@
+﻿namespace CinemaApp.WebApi.Controllers
+{
+    using System.Security.Claims;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("[controller]")]
+    public abstract class BaseExternalApiController : ControllerBase
+    {
+        protected bool IsUserAuthenticated()
+        {
+            bool retRes = false;
+            if (this.User.Identity != null)
+            {
+                retRes = this.User.Identity.IsAuthenticated;
+            }
+
+            return retRes;
+        }
+
+        protected string? GetUserId()
+        {
+            string? userId = null;
+            if (this.IsUserAuthenticated())
+            {
+                userId = this.User
+                    .FindFirstValue(ClaimTypes.NameIdentifier);
+            }
+
+            return userId;
+        }
+    }
+}
---
+++ CinemaApp.WebApi/Controllers/CinemaMovieApiController.cs
@@ -0,0 +1,42 @@
+﻿namespace CinemaApp.WebApi.Controllers
+{
+    using System.ComponentModel.DataAnnotations;
+    using Microsoft.AspNetCore.Mvc;
+
+    using Services.Core.Interfaces;
+    
+    public class CinemaMovieApiController : BaseExternalApiController
+    {
+        private readonly IProjectionService projectionService;
+
+        public CinemaMovieApiController(IProjectionService projectionService)
+        {
+            this.projectionService = projectionService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [Route("Showtimes")]
+        public async Task<ActionResult<IEnumerable<string>>> GetProjectionShowtimes([Required]string cinemaId, [Required]string movieId)
+        {
+            IEnumerable<string> showtimes = await this.projectionService
+                .GetProjectionShowtimesAsync(cinemaId, movieId);
+
+            return this.Ok(showtimes);
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [Route("AvailableTickets")]
+        public async Task<ActionResult<int>> GetAvailableTickets([Required]string cinemaId,
+            [Required] string movieId, [Required] string showtime)
+        {
+            int availableTickets = await this.projectionService
+                .GetAvailableTicketsCountAsync(cinemaId, movieId, showtime);
+
+            return this.Ok(availableTickets);
+        }
+    }
+}
